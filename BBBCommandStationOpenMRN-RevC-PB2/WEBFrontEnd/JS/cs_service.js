923dce5f92661378571bdceac7b015cdd7416511
Robert Heller <heller@deepsoft.com>
2026-03-18 15:59:52 -0400

    Static web files & javascript.

---
+++ BBBCommandStationOpenMRN-RevC-PB2/WEBFrontEnd/JS/cs_service.js
@@ -0,0 +1,166 @@
+/* -*- javascript -*- 
+     Copyright 2026 Robert Heller
+     All Rights Reserved
+     System        : $BUFFER_NAME$ : 
+     Object Name   : $RCS_FILE$
+     Revision      : $REVISION$
+     Date          : 2026-03-18 13:32:23
+     Created By    : Robert Heller, Robert Heller
+     Created       : 2026-03-18 13:32:23
+
+     Last Modified : <260318.1529>
+     ID            : $Id$
+     Source        : $Source$
+     Description	
+     Notes
+*/
+var ns4 = document.layers;
+var ie4 = document.all;
+var nn6 = document.getElementById && !document.all;
+
+var CS_Service = {};
+
+CS_Service.SelectShortAddress = function () {
+}
+
+CS_Service.SelectLongAddress = function () {
+}
+
+CS_Service.UpdateAddress = function () {
+}
+
+CS_Service.UpdatePrimaryAddress = function () {
+}
+
+CS_Service.UpdateExtendedAddress = function () {
+}
+
+CS_Service.SelectAddressFormat = function () {
+}
+
+CS_Service.SelectDirection = function () {
+}
+
+CS_Service.SelectSpeedSteps = function () {
+}
+
+CS_Service.SelectPowerConversion = function () {
+}
+
+CS_Service.SelectKickStart = function () {
+}
+
+CS_Service.SelectAccelerationRate = function () {
+}
+
+CS_Service.SelectDecelerationRate = function () {
+}
+
+CS_Service.SelectMotorFreq = function () {
+}
+
+CS_Service.SelectBasicSpeed = function () {
+}
+
+CS_Service.SelectVStart = function () {
+}
+
+CS_Service.SelectVMid = function () {
+}
+
+CS_Service.SelectVHigh = function () {
+}
+
+CS_Service.SelectSpeedTable = function () {
+}
+
+CS_Service.SpeedTableBar = function(bar) {
+}
+
+CS_Service.SpeedTableCheckbox = function(bar) {
+}
+
+CS_Service.ServiceForceStraight = function () {
+}
+
+CS_Service.ServiceMatchEnds = function () {
+}
+
+CS_Service.ServiceConstantRatioCurve = function () {
+}
+
+CS_Service.ServiceLogCurve = function () {
+}
+
+CS_Service.ServiceShiftLeft = function () {
+}
+
+CS_Service.ServiceShiftRight = function () {
+}
+
+CS_Service.SelectFunctionMap = function(funct,wire,forward=true) {
+}
+
+CS_Service.SelectDitchLightHold = function () { 
+}
+
+CS_Service.SelectLightingEffectsPage = function () {
+}
+
+CS_Service.SelectOutputEffectGenerated = function (output) {
+}
+
+CS_Service.SelectOutputActive = function (output) { 
+}
+
+CS_Service.SelectOutputConnected = function (output) {
+}
+
+CS_Service.SelectPowerSourceConversion = function () {
+}
+
+CS_Service.SelectConsistAddress = function () {
+}
+
+CS_Service.SelectDirectionInConsist = function () {                                 
+}
+
+CS_Service.ReadCV = function (cv) {
+}
+
+CS_Service.WriteCV = function (cv) {
+}
+
+CS_Service.CompareCV = function (cv) {
+}
+
+CS_Service.ReadChagesOnSheet = function () {
+}
+
+CS_Service.WriteChagesOnSheet = function () {
+}
+
+CS_Service.CompareChagesOnSheet = function () {
+}
+
+CS_Service.ReadFullSheet = function () {
+}
+
+CS_Service.WriteFullSheet = function () {
+}
+
+CS_Service.CompareFullSheet = function () {
+}
+
+CS_Service.ReadChagesOnAllSheets = function () {
+}
+
+CS_Service.WriteChagesOnAllSheets = function () {
+}
+
+CS_Service.ReadAllSheets = function () {
+}
+
+CS_Service.WriteAllSheets = function () {
+}
+
