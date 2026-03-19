5e980293628ee115b91460df4a6ff41a0748c6a1
Robert Heller <heller@deepsoft.com>
2026-03-19 15:19:03 -0400

    Working on Programming Track Javascript code.

--- BBBCommandStationOpenMRN-RevC-PB2/WEBFrontEnd/JS/cs_service.js
+++ BBBCommandStationOpenMRN-RevC-PB2/WEBFrontEnd/JS/cs_service.js
@@ -8,7 +8,7 @@
      Created By    : Robert Heller, Robert Heller
      Created       : 2026-03-18 13:32:23
 
-     Last Modified : <260318.2321>
+     Last Modified : <260319.1518>
      ID            : $Id$
      Source        : $Source$
      Description	
@@ -21,7 +21,7 @@ var nn6 = document.getElementById && !document.all;
 var CS_Service = {};
 
 CS_Service.SelectShortAddress = function () {
-  console.log("*** SelectShortAddress()");
+  //console.log("*** SelectShortAddress()");
   /* Selects short address.  Tasks when selected:
    * CV 29 bit 5 = 0
    * document.getElementById("address").value = document.getElementById("primary-address").value
@@ -44,7 +44,7 @@ CS_Service.SelectShortAddress = function () {
 }
 
 CS_Service.SelectLongAddress = function () {
-  console.log("*** SelectLongAddress()");
+  //console.log("*** SelectLongAddress()");
   /* Selects long address.  Tasks when selected: 
    * CV 29 bit 5 = 1
    * document.getElementById("address").value = document.getElementById("extended-address").value
@@ -75,7 +75,7 @@ CS_Service.SelectLongAddress = function () {
 }
 
 CS_Service.UpdateAddress = function () {
-  console.log("*** UpdateAddress()");
+  //console.log("*** UpdateAddress()");
   /*
    * Update address case on "address-format"
    * if document.getElementById("address-format").value == "long", update:
@@ -106,7 +106,7 @@ CS_Service.UpdateAddress = function () {
 }
 
 CS_Service.UpdatePrimaryAddress = function () {
-  console.log("*** UpdatePrimaryAddress()");
+  //console.log("*** UpdatePrimaryAddress()");
   var address = document.getElementById("primary-address");
   var addr = address.value;
   if (document.getElementById("address-format").value == "short")
@@ -119,7 +119,7 @@ CS_Service.UpdatePrimaryAddress = function () {
 }
 
 CS_Service.UpdateExtendedAddress = function () {
-  console.log("*** UpdateExtendedAddress()");
+  //console.log("*** UpdateExtendedAddress()");
   var address = document.getElementById("extended-address");
   var addr = address.value;
   if (document.getElementById("address-format").value == "long")
@@ -134,7 +134,7 @@ CS_Service.UpdateExtendedAddress = function () {
 }
 
 CS_Service.SelectAddressFormat = function () {
-  console.log("*** SelectAddressFormat()");
+  //console.log("*** SelectAddressFormat()");
   if (document.getElementById("address-format").value == "long")
   {
     /* Selects long address.  Tasks when selected: 
@@ -191,55 +191,185 @@ CS_Service.SelectAddressFormat = function () {
 }
 
 CS_Service.SelectDirection = function () {
-  console.log("*** SelectDirection()");
+  //console.log("*** SelectDirection()");
+  /* set loco dir
+   * CV_29 bit 0: normal == 0, reverse == 1
+   */
+  var CV_29 = document.getElementById("CV29_Value");
+  var CV_29s = document.getElementById("CV29_State");
+  var dir = document.getElementById("direction");
+  if (dir.value == "normal")
+  {
+    CV_29.value &= ~1;
+  }
+  else
+  {
+    CV_29.value |= 1;
+  }
+  CV_29s.innerHTML = "Edited";
 }
 
 CS_Service.SelectSpeedSteps = function () {
-  console.log("*** SelectSpeedSteps()");
+  //console.log("*** SelectSpeedSteps()");
+  /* 14 vs 28/256 speed steps: CV_29 bit 1: 0 = 14, 1 = 28/256 */
+  var CV_29 = document.getElementById("CV29_Value");
+  var CV_29s = document.getElementById("CV29_State");
+  var steps = document.getElementById("steps");
+  if (steps.value == "14")
+  {
+    CV_29.value &= ~(1 << 1);
+  }
+  else
+  {
+    CV_29.value |= (1 << 1);
+  }
+  CV_29s.innerHTML = "Edited";
 }
 
 CS_Service.SelectPowerConversion = function () {
-  console.log("*** SelectPowerConversion()");
+  //console.log("*** SelectPowerConversion()");
+  /* CV_29 bit 2: 0 = NMRA Digital Only, 1 = DC conversion */
+  var CV_29 = document.getElementById("CV29_Value");
+  var CV_29s = document.getElementById("CV29_State");
+  var power = document.getElementById("power");
+  if (power.value = "dcc")
+  {
+    CV_29.value &= ~(1 << 2);
+  }
+  else
+  {
+    CV_29.value |= (1 << 2);
+  }
+  CV_29s.innerHTML = "Edited";
 }
 
 CS_Service.SelectKickStart = function () {
-  console.log("*** SelectKickStart()");
+  //console.log("*** SelectKickStart()");
+  /* CV 65 */
+  var CV_65 = document.getElementById("CV65_Value");
+  var CV_65s = document.getElementById("CV65_State");
+  var kick = document.getElementById("kick");
+  CV_65.value = kick.value;
+  CV_65s.innerHTML = "Edited";
 }
 
 CS_Service.SelectAccelerationRate = function () {
-  console.log("*** SelectAccelerationRate()");
+  //console.log("*** SelectAccelerationRate()");
+  /* CV 3 */
+  var CV_3 = document.getElementById("CV3_Value");
+  var CV_3s = document.getElementById("CV3_State");
+  var accel = document.getElementById("accel");
+  CV_3.value = accel.value;
+  CV_3s.innerHTML = "Edited";
 }
 
 CS_Service.SelectDecelerationRate = function () {
-  console.log("*** SelectDecelerationRate()");
+  //console.log("*** SelectDecelerationRate()");
+  /* CV 4 */
+  var CV_4 = document.getElementById("CV4_Value");
+  var CV_4s = document.getElementById("CV4_State");
+  var decel = document.getElementById("decel");
+  CV_4.value = decel.value;
+  CV_4s.innerHTML = "Edited"; 
 }
 
 CS_Service.SelectMotorFreq = function () {
-  console.log("*** SelectMotorFreq()");
+  //console.log("*** SelectMotorFreq()");
+  /* CV 9 (PWM) */
+  var CV_9 = document.getElementById("CV9_Value");
+  var CV_9s  = document.getElementById("CV9_State"); 
+  var freq = document.getElementById("freq");
+  CV_9.value = freq.value; 
+  CV_9s.innerHTML = "Edited";
 }
 
 CS_Service.SelectBasicSpeed = function () {
-  console.log("*** SelectBasicSpeed()");
+  //console.log("*** SelectBasicSpeed()");
+  /* CV 29 bit 4: 0 == basic speed */
+  var CV_29 = document.getElementById("CV29_Value"); 
+  var CV_29s = document.getElementById("CV29_State");
+  CV_29.value &= ~(1 << 4);
+  CV_29s.innerHTML = "Edited";
 }
 
 CS_Service.SelectVStart = function () {
-  console.log("*** SelectVStart()");
+  //console.log("*** SelectVStart()");
+  /* CV 2 */
+  var CV_2 = document.getElementById("CV2_Value");
+  var CV_2s = document.getElementById("CV2_State");
+  var vstart = document.getElementById("Vstart");
+  CV_2.value = vstart.value; 
+  CV_2s.innerHTML = "Edited";
 }
 
 CS_Service.SelectVMid = function () {
-  console.log("*** SelectVMid()");
+  //console.log("*** SelectVMid()");
+  /* CV 6 */
+  var CV_6 = document.getElementById("CV6_Value");
+  var CV_6s = document.getElementById("CV6_State");
+  var vmid = document.getElementById("Vmid");
+  CV_6.value = vmid.value;
+  CV_6s.innerHTML = "Edited";
 }
 
 CS_Service.SelectVHigh = function () {
-  console.log("*** SelectVHigh()");
+  //console.log("*** SelectVHigh()");
+  /* CV 5 */
+  var CV_5 = document.getElementById("CV5_Value");
+  var CV_5s = document.getElementById("CV5_State");
+  var vhigh = document.getElementById("Vhigh");
+  CV_5.value = vhigh.value;
+  CV_5s.innerHTML = "Edited";
 }
 
 CS_Service.SelectSpeedTable = function () {
-  console.log("*** SelectSpeedTable()");
+  //console.log("*** SelectSpeedTable()");
+  /* CV_29 bit 4: 1 == speed table */
+  var CV_29 = document.getElementById("CV29_Value"); 
+  var CV_29s = document.getElementById("CV29_State");
+  CV_29.value |= (1 << 4);
+  CV_29s.innerHTML = "Edited";
 }
 
-CS_Service.SpeedTableBar = function(bar) {
-  console.log("*** SpeedTableBar()");
+CS_Service.SpeedTableBar = function(barnumber) {
+  //console.log("*** SpeedTableBar("+barnumber+")");
+  var bar = document.getElementById("b"+barnumber);
+  var barVal = document.getElementById("b"+barnumber+"Value");
+  var CV =  document.getElementById("CV"+(barnumber+65)+"_Value");
+  var CVs =  document.getElementById("CV"+(barnumber+65)+"_State");
+  var val = Number.parseInt(bar.value);
+  CV.value = val;
+  CVs.innerHTML = "Edited";
+  barVal.innerHTML = val;
+  //console.log("*** SpeedTableBar(): val is "+val);
+  for (var next = barnumber+1; next <= 28; next++)
+  {
+    //console.log("*** SpeedTableBar(): next (going up) is "+next);
+    var nextbar = document.getElementById("b"+next);
+    var nextbarValue = document.getElementById("b"+next+"Value");
+    var nextCV = document.getElementById("CV"+(next+65)+"_Value");
+    var nextCVs = document.getElementById("CV"+(next+65)+"_State");
+    //console.log("*** SpeedTableBar(): [up] nextbar.value is "+nextbar.value);
+    if (val < Number.parseInt(nextbar.value)) break;
+    nextbar.value = val;
+    nextbarValue.innerHTML = val;
+    nextCV.value = val;
+    nextCVs.innerHTML = "Edited";
+  }
+  for (next = barnumber-1; next >= 1; next--)
+  {
+    //console.log("*** SpeedTableBar(): next (going down) is "+next);
+    var nextbar = document.getElementById("b"+next);
+    var nextbarValue = document.getElementById("b"+next+"Value");
+    var nextCV = document.getElementById("CV"+(next+65)+"_Value");
+    var nextCVs = document.getElementById("CV"+(next+65)+"_State");
+    //console.log("*** SpeedTableBar(): [down] nextbar.value is "+nextbar.value);
+    if (val > Number.parseInt(nextbar.value)) break;
+    nextbar.value = val;
+    nextbarValue.innerHTML = val;
+    nextCV.value = val;
+    nextCVs.innerHTML = "Edited";
+  }
 }
 
 CS_Service.SpeedTableCheckbox = function(bar) {
@@ -247,27 +377,128 @@ CS_Service.SpeedTableCheckbox = function(bar) {
 }
 
 CS_Service.ServiceForceStraight = function () {
-  console.log("*** ServiceForceStraight()");
+  //console.log("*** ServiceForceStraight()");
+  var fraction = 255.0 / 27.0;
+  for (var i = 1; i <= 28; i++)
+  {
+    var val = ((i-1)*fraction).toFixed();
+    var bar = document.getElementById("b"+i);
+    var barVal = document.getElementById("b"+i+"Value");
+    var CV =  document.getElementById("CV"+(i+65)+"_Value"); 
+    var CVs =  document.getElementById("CV"+(i+65)+"_State");
+    CV.value = val;
+    CVs.innerHTML = "Edited";
+    barVal.innerHTML = val;
+    bar.value = val;
+  }
+     
 }
 
 CS_Service.ServiceMatchEnds = function () {
-  console.log("*** ServiceMatchEnds()");
+  //console.log("*** ServiceMatchEnds()");
+  var lowest = Number.parseInt(document.getElementById("b1").value);
+  //console.log("*** ServiceMatchEnds(): lowest = "+lowest);
+  var highest = Number.parseInt(document.getElementById("b28").value);
+  //console.log("*** ServiceMatchEnds(): highest = "+highest);
+  var fraction = (highest-lowest) / 27.0;
+  //console.log("*** ServiceMatchEnds(): fraction = "+fraction);
+  for (var i = 1; i <= 28; i++)
+  {
+    var val = (((i-1)*fraction)+lowest).toFixed();
+    //console.log("*** ServiceMatchEnds(): @"+i+", val = "+val);
+    var bar = document.getElementById("b"+i);
+    var barVal = document.getElementById("b"+i+"Value");
+    var CV =  document.getElementById("CV"+(i+65)+"_Value"); 
+    var CVs =  document.getElementById("CV"+(i+65)+"_State");
+    CV.value = val;
+    CVs.innerHTML = "Edited";
+    barVal.innerHTML = val;
+    bar.value = val;
+  }
+  
 }
 
 CS_Service.ServiceConstantRatioCurve = function () {
-  console.log("*** ServiceConstantRatioCurve()");
+  //console.log("*** ServiceConstantRatioCurve()");
+  var first = Number.parseFloat(document.getElementById("b1").value);
+  if (first < 1.0) first = 1.0;
+  var last = Number.parseFloat(document.getElementById("b28").value);
+  if (last < first + 1.0) last = first + 1.0;
+  step = Math.log(last / first) / 27.0;
+  //console.log("*** ServiceConstratioRatioCurve(): log ratio step is "+step);
+  for (var i = 1; i <= 28; i++)
+  {
+    var val = Math.floor(first * Math.exp(step * (i-1))).toFixed();
+    var bar = document.getElementById("b"+i);
+    var barVal = document.getElementById("b"+i+"Value");
+    var CV =  document.getElementById("CV"+(i+65)+"_Value"); 
+    var CVs =  document.getElementById("CV"+(i+65)+"_State");
+    CV.value = val;
+    CVs.innerHTML = "Edited";
+    barVal.innerHTML = val;
+    bar.value = val;
+  }
 }
 
 CS_Service.ServiceLogCurve = function () {
-  console.log("*** ServiceLogCurve()");
+  //console.log("*** ServiceLogCurve()");
+  var first = Number.parseFloat(document.getElementById("b1").value);
+  var last = Number.parseFloat(document.getElementById("b28").value);
+  if (last < first + 1.0) last = first + 1.0;
+  var factor = 1.0 / 10.0;
+  var ratio = Math.pow(1.0 - factor, 27.0);
+  var limit = last + (last - first) * ratio;
+  for (var i = 2; i <= 28; i++)
+  {
+    var previous = limit - (limit - first) * ratio / Math.pow(1. - factor, 27.0 - (i-1));
+    var val = Math.floor(previous).toFixed();
+    var bar = document.getElementById("b"+i);
+    var barVal = document.getElementById("b"+i+"Value");
+    var CV =  document.getElementById("CV"+(i+65)+"_Value"); 
+    var CVs =  document.getElementById("CV"+(i+65)+"_State");
+    CV.value = val;
+    CVs.innerHTML = "Edited";
+    barVal.innerHTML = val;
+    bar.value = val;
+  }
+  
 }
 
 CS_Service.ServiceShiftLeft = function () {
-  console.log("*** ServiceShiftLeft()");
+  //console.log("*** ServiceShiftLeft()");
+  var val = Number.parseInt(document.getElementById("b28").value);
+  for (var i = 27; i >= 1; i--)
+  {
+    var nextval = Number.parseInt(document.getElementById("b"+i).value);
+    var bar = document.getElementById("b"+i);
+    var barVal = document.getElementById("b"+i+"Value");
+    var CV =  document.getElementById("CV"+(i+65)+"_Value"); 
+    var CVs =  document.getElementById("CV"+(i+65)+"_State");
+    CV.value = val;
+    CVs.innerHTML = "Edited";
+    barVal.innerHTML = val;
+    bar.value = val;
+    val = nextval;
+
+  }
 }
 
 CS_Service.ServiceShiftRight = function () {
-  console.log("*** ServiceShiftRight()");
+  //console.log("*** ServiceShiftRight()");
+  var val = Number.parseInt(document.getElementById("b1").value);
+  for (var i = 2; i <= 28; i++)
+  {
+    var nextval = Number.parseInt(document.getElementById("b"+i).value);
+    var bar = document.getElementById("b"+i);
+    var barVal = document.getElementById("b"+i+"Value");
+    var CV =  document.getElementById("CV"+(i+65)+"_Value"); 
+    var CVs =  document.getElementById("CV"+(i+65)+"_State");
+    CV.value = val;
+    CVs.innerHTML = "Edited";
+    barVal.innerHTML = val;
+    bar.value = val;
+    val = nextval;
+  }
 }
 
 CS_Service.SelectFunctionMap = function(funct,wire,forward=true) {
